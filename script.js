f9546039b894c46430ceebaeeca60e96a6d232ef
DA MAC 50 <damac50@academy-learners-MacBook-Air.local>
2022-07-11 12:05:57 +0200

    added smooties

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('hello')
+console.log('hello');
+
+//alert('hello this is Qazi');
+
+
+var b = 'smooties'
+console.log(b);
+
+
+var someNumber = 45;
+//console.log(someNumber);
+
+//document.getElementById
+
+var num1 =10;
+num1 ++;
+
+num1 --;
+console.log(num1);
+
+console.log(num1 % 6);
